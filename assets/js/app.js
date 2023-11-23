2b26dc3fbf50f2a4d2350240326c187877f365fb
ManishSharma-17 <manishsincere17@gmail.com>
2023-11-23 10:38:04 +0530

    navbar setting with js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const menulist = document.querySelector(".menuList")
 const menuIcon = document.querySelector("#menuIcon");
+const nav = document.querySelector("nav");
 menuIcon.addEventListener("click", () => {
     menulist.classList.toggle("show");
+    document.body.classList.toggle("sm-overflow-hidden");
 })
